5a6203a2b7e3c353f3c7eeabb1f9e2fe6cea6f8c
Doğukan <dogukandogu8@gmail.com>
2020-09-12 01:31:21 +0300

    Create formpython.py

---
+++ formpython.py
@@ -0,0 +1,8 @@
+import tkinter
+def createWindow():
+    window =tkinter.Tk()
+    window.title("Network Managment System")
+    window.geometry("500x500+100+200")
+    window.mainloop()
+if __name__=='__main__':
+    createWindow()
